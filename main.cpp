802466a3140b5ebedca54149c56802bf9eae15a1
Alidal <vladyslav.sydorenko@gmail.com>
2015-03-09 17:46:35 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// SCompiler.cpp : Defines the entry point for the console application.
+//
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "Hello, World! \n";
+	return 0;
+}
